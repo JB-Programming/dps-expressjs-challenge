3d37a27fa325187d34f67be1d5f1cb429e49107f
JB-Programming <jb.programming.21@gmail.com>
2026-01-11 18:05:17 +0100

    Added create player (post)

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Express } from 'express';
 import dotenv from 'dotenv';
 import { initializeDatabase } from './services/init-db.service';
 import tournamentRoutes from './routes/tournament.routes';
+import playerRoutes from './routes/player.routes';
 
 dotenv.config();
 
@@ -21,6 +22,7 @@ app.get('/health', (req, res) => {
 
 // Routes
 app.use('/tournaments', tournamentRoutes);
+app.use('/players', playerRoutes);
 
 app.listen(port, () => {
 	console.log(`[server]: Server is running at http://localhost:${port}`);
