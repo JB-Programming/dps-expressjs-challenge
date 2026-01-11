db48ca7df916a840f5e5631dcfea7bfddc3df98c
JB-Programming <jb.programming.21@gmail.com>
2026-01-11 20:36:04 +0100

    Added Player participation (Post)

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import { initializeDatabase } from './services/init-db.service';
 import tournamentRoutes from './routes/tournament.routes';
 import playerRoutes from './routes/player.routes';
+import participantRoutes from './routes/participant.routes';
 
 dotenv.config();
 
@@ -23,6 +24,7 @@ app.get('/health', (req, res) => {
 // Routes
 app.use('/tournaments', tournamentRoutes);
 app.use('/players', playerRoutes);
+app.use('/participants', participantRoutes);
 
 app.listen(port, () => {
 	console.log(`[server]: Server is running at http://localhost:${port}`);
